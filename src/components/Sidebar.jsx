bfed719b218e74255764bcb23f4d9e5620c0f130
heyyakash <akashsharma2002@gmail.com>
2023-05-11 14:13:27 +0530

    made scroll bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import { AiFillHome } from 'react-icons/ai'
 import { BiErrorCircle } from 'react-icons/bi'
-import {CgDarkMode} from "react-icons/cg"
+import { CgDarkMode } from "react-icons/cg"
 
-const Sidebar = ({ vertical, horizontal, dict,setVertical,setHorizontal }) => {
+const Sidebar = ({ vertical, horizontal, dict, setVertical, setHorizontal }) => {
     const [result, setResult] = useState([])
     const [error, setError] = useState(null)
 
@@ -11,7 +11,7 @@ const Sidebar = ({ vertical, horizontal, dict,setVertical,setHorizontal }) => {
         const html = document.querySelector("html")
         html.classList.toggle("dark")
     }
-    
+
 
     const getResult = () => {
         setResult([])
@@ -53,16 +53,16 @@ const Sidebar = ({ vertical, horizontal, dict,setVertical,setHorizontal }) => {
         <div className='h-screen w-[400px] flex bg-white dark:bg-black/90 dark:text-white  flex-col'>
             <div className='w-full font-bold  dark:border-none px-5 py-6  flex items-center justify-between'>
                 <h2 className='text-2xl '>House Selection</h2>
-                <div onClick={()=>toggleDarkMode()} className='rounded-full h-8 w-8 cursor-pointer trans hover:bg-white hover:text-black border grid place-items-center text-xl'>
+                <div onClick={() => toggleDarkMode()} className='rounded-full h-8 w-8 cursor-pointer trans hover:bg-white hover:text-black border grid place-items-center text-xl'>
                     <CgDarkMode />
                 </div>
             </div>
             <div className='flex flex-col gap-3 p-5 w-[90%] self-center rounded-lg dark:bg-white/20 bg-black/10'>
                 <h3 className='text-xl font-bold'>Set Dimensions</h3>
                 <label htmlFor="v mt-5">Veritcal</label>
-                <input id = "v" type="number" min={1} value={vertical} onChange={(e) => setVertical(e.target.value)} placeholder='veritical' className='input-primary' />
+                <input id="v" type="number" min={1} value={vertical} onChange={(e) => setVertical(e.target.value)} placeholder='veritical' className='input-primary' />
                 <label htmlFor="h">Horizontal</label>
-                <input id = "h" type="number" min={1} value={horizontal} onChange={(e) => setHorizontal(e.target.value)} placeholder='Horizontal' className='input-primary' />
+                <input id="h" type="number" min={1} value={horizontal} onChange={(e) => setHorizontal(e.target.value)} placeholder='Horizontal' className='input-primary' />
             </div>
             <div className='p-5'>
                 <button onClick={() => getResult()} className='bg-purple-500 disabled:bg-opacity-50 text-white trans p-2 hover:bg-white hover:text-purple-500   font-semibold rounded-lg  w-full'> Suggest House</button>
@@ -71,18 +71,24 @@ const Sidebar = ({ vertical, horizontal, dict,setVertical,setHorizontal }) => {
                 <BiErrorCircle />
                 {error}</p>
             }
-            {result[0] ? (
-                <div className='flex flex-col gap-3 px-5'>
-                    <p className='text-sm text-gray-500'>Best house</p>
-                    <div className='flex items-center gap-2'>
-                        <AiFillHome className='text-2xl text-green-500' />
-                        <p className='text-xl font-semibold'>{result[0].label}</p>
-                    </div>
-                    <p>Distance from Gym : <b>{result[0].GymScore}km</b></p>
-                    <p>Distance from Restaurant : <b>{result[0].RestaurantScore}km</b></p>
-                    <p>Distance from Hospital : <b> {result[0].HospitalScore}km </b></p>
-                </div>
-            ) : (<></>)}
+            <p className='text-sm text-gray-500 px-5 mb-3 '>Best house</p>
+            <div className='flex flex-col gap-5 px-5  overflow-y-auto py-5'>
+                {result.map(x => {
+                    return (
+                        <div key={x.label} className='flex flex-col gap-2 px-3 py-3  first:dark:bg-green-400/20 first:bg-green-400/20 dark:bg-white/20 bg-black/10 rounded-md'>
+
+                            <div className='flex items-center gap-2'>
+                                <AiFillHome className='text-2xl text-green-500' />
+                                <p className='text-xl font-semibold'>{x.label}</p>
+                            </div>
+                            <p>Distance from Gym : <b>{x.GymScore}km</b></p>
+                            <p>Distance from Restaurant : <b>{x.RestaurantScore}km</b></p>
+                            <p>Distance from Hospital : <b> {x.HospitalScore}km </b></p>
+                        </div>
+                    )
+                })}
+            </div>
+
         </div>
     )
 }
